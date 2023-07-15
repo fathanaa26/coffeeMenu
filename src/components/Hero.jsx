a3ee06875f1805604dceccaa932f38f462c6f3c0
fathanaa26 <fathan.aa.01@gmail.com>
2023-07-15 19:55:03 +0700

    3 files modified, 4 added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,28 +3,26 @@ import { useEffect, useState } from "react";
 const data = [
   {
     id: 0,
-    title: "Title1",
-    desc: " Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem q uasi. In deleniti eaque aut repudiandae et a id nisi. ",
+    title: "Civil engineering",
+    desc: "Civil engineering is a branch of engineering that deals with the design, construction, and maintenance of infrastructure, including buildings, highways, bridges, water and sewage systems, and other structures.",
     imgurl: "http://placekitten.com/g/1080/1920",
   },
   {
     id: 1,
-    title: "Title2",
-    desc: "essor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of",
+    title: "Architecture",
+    desc: "Architecture defines the design and construction of buildings and other physical structures. It merges aesthetics with functionality, with the goal of creating efficient, safe, and visually appealing spaces for human use.",
     imgurl: "http://placekitten.com/g/1081/1921",
   },
   {
     id: 2,
-    title: "Title3",
-    desc: " of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bo",
+    title: "Soil Analyze",
+    desc: "Soil analysis is a process that examines the chemical, physical and biological composition of soil. This process involves collecting soil samples from different locations, testing them for pH levels, organic matter content, nutrient levels, and the presence of contaminants.",
     imgurl: "http://placekitten.com/g/1082/1922",
   },
 ];
 
 export default function Hero() {
-  let i = 0;
-  const [count, setCount] = useState(i);
-  const [text, setText] = useState();
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -34,20 +32,22 @@ export default function Hero() {
         }
         return e + 1;
       });
-    }, 1000);
+    }, 7000);
 
     return () => clearInterval(intervalId);
   }, []);
 
   return (
     <>
-      <div className="hero min-h-screen">
+      <div
+        className="hero min-h-screen"
+        style={{ backgroundImage: `url(${data[count].imgurl})` }}
+      >
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-neutral-content">
-          <div className="min-w-sm">
-            <h1>{count}</h1>
-            <h1 className="mb-5 text-5xl text-white">{data[0].title}</h1>
-            <p className="mb-5">{data[1].desc}</p>
+          <div className="flex flex-col max-w-sm">
+            <h1 className="mb-5 text-5xl text-white">{data[count].title}</h1>
+            <p className="mb-5">{data[count].desc}</p>
           </div>
         </div>
       </div>
