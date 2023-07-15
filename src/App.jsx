50041d0a6ee82e984ea4fb506f41804521982078
fathanaa26 <fathan.aa.01@gmail.com>
2023-07-15 21:39:58 +0700

    1 file modified, 1 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Footer from "./components/Footer.jsx";
+import DummyFormInput from "./components/DummyFormInput.jsx";
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <DummyFormInput />
       <Footer />
     </>
   );
