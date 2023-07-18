606158a7dc2fbec69b959bd3a6a7173d25eb3a6a
Fathan Akbar A <fathan.aa.01@gmail.com>
2023-07-18 18:07:11 +0700

    kontak-form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,34 @@ export default function ContactPage() {
   return (
     <>
       <Navbar />
-      <h1 className="min-h-screen">Contact Page</h1>
+      <div className="bg-slate-300 min-h-screen">
+        <div className="p-6 mx-auto w-full md:max-w-3xl">
+          <form action="/" method="post" className="flex flex-row gap-4">
+            <input
+              type="text"
+              className="input bg-white input-bordered w-full"
+              name="name"
+              id="name"
+              placeholder="Name"
+            />
+            <input
+              type="email"
+              className="input bg-white input-bordered w-full"
+              name="email"
+              id="email"
+              placeholder="E-Mail"
+            />
+          </form>
+          <textarea
+            placeholder="Type your question here"
+            className="textarea textarea-bordered textarea-md w-full mt-4 bg-white"
+          ></textarea>
+          <button type="submit" className=" btn btn-wide mt-2 text-white">
+            Submit
+          </button>
+        </div>
+      </div>
+
       <Footer />
     </>
   );
